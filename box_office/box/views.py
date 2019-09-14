16787bf078d7fc008d1a8736a864c339a408c264
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-14 14:09:03 +0900

    first commiut

---
+++ box_office/box/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.request import Request
+import requests
+import json
+
+class Daily_Box(APIView):
+
+    def post(self, request:Request):
+        pass
+
+class Weekly_Box(APIView):
+
+    def post(self, request:Request):
+        pass
+
+
+
+# Create your views here.
