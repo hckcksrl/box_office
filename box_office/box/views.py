ae0b6b6248d6ed2a855ce4f2ee77e874576b9988
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-14 16:09:36 +0900

    api key

--- box_office/box/views.py
+++ box_office/box/views.py
@@ -5,11 +5,26 @@ from rest_framework import status
 from rest_framework.request import Request
 import requests
 import json
+import datetime
+
+with open('/Users/hckcksrl/Desktop/study/box_office/box_office/box/config.json', 'r') as f:
+    config = json.load(f)
+
+api_key = config['api_key']
+
 
 class Daily_Box(APIView):
 
-    def post(self, request:Request):
-        pass
+    def get_box_office(self):
+        todey = datetime.datetime.now().strftime('%Y%m%d')
+        date = str(int(todey) - 1)
+        api = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={api_key}&targetDt={date}'
+        data = requests.get(api)
+        return data.json()
+
+    def get(self, request:Request):
+        box = self.get_box_office()
+        return Response(status=status.HTTP_200_OK)
 
 class Weekly_Box(APIView):
 
