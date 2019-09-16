b6ee1572e3268d027812d98f4d5f524d47fcf962
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-16 19:19:00 +0900

    commit

--- box_office/box/views.py
+++ box_office/box/views.py
@@ -82,15 +82,15 @@ class Daily_Box(APIView):
                                 "description": "감독 : 손용호\n출연 : 마동석|김상중|김아중|장기용|\n평점 : 6.93",
                                 "thumbnail": {
                                     "imageUrl": "https://ssl.pstatic.net/imgmovie/mdi/mit110/1779/177909_P32_103957.jpg",
-                            #         "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=177909"
+                                    "link": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=177909"
                                 },
-                            #     "buttons": [
-                            #     {
-                            #         "action": "webLink",
-                            #         "label": "사이트 이동",
-                            #         "webLinkUrl": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=177909"
-                            #     }
-                            #     ]
+                                "buttons": [
+                                {
+                                    "action": "webLink",
+                                    "label": "사이트 이동",
+                                    "webLinkUrl": "https://movie.naver.com/movie/bi/mi/basic.nhn?code=177909"
+                                }
+                                ]
                             }
                         ]
                     }
