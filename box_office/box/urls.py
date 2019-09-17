ac997ee8989edf23304dea38caca5e3e9c7e5342
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-09-17 16:08:48 +0900

    weekly , weekend

--- box_office/box/urls.py
+++ box_office/box/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('daily', views.Daily_Box.as_view()),
-    path('search', views.Movie_Search.as_view())
+    path('search', views.Movie_Search.as_view()),
+    path('weekly', views.Weekly_Box.as_view())
 ]
